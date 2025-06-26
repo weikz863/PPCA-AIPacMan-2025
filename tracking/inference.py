72f8923e562aebda3dd7cf714d2c8d1a55b4010a
weikz <weikz@sjtu.edu.cn>
2025-06-26 16:04:34 +0800

    feat: add constructBayesNet

--- tracking/inference.py
+++ tracking/inference.py
@@ -61,7 +61,13 @@ def constructBayesNet(gameState: hunters.GameState):
     variableDomainsDict = {}
 
     "*** YOUR CODE HERE ***"
-    raiseNotDefined()
+    variables = [PAC, GHOST0, GHOST1, OBS0, OBS1]
+    edges = [(PAC, OBS0), (PAC, OBS1), (GHOST0, OBS0), (GHOST1, OBS1)]
+    variableDomainsDict = {PAC: [(x, y) for x in range(X_RANGE) for y in range(Y_RANGE)], 
+                           GHOST0: [(x, y) for x in range(X_RANGE) for y in range(Y_RANGE)],
+                           GHOST1: [(x, y) for x in range(X_RANGE) for y in range(Y_RANGE)],
+                           OBS0: list(range(X_RANGE + Y_RANGE + MAX_NOISE - 1)),
+                           OBS1: list(range(X_RANGE + Y_RANGE + MAX_NOISE - 1)),}
     "*** END YOUR CODE HERE ***"
 
     net = bn.constructEmptyBayesNet(variables, edges, variableDomainsDict)
